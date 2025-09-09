f24f182514680d55486f6abd2f9ed1635bef2132
Disha <127115356+undeadcreature@users.noreply.github.com>
2025-09-09 10:19:55 +0530

    Updated store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -9,26 +9,12 @@ import jobsSlice from './slices/jobsSlice';
 import mentorshipSlice from './slices/mentorshipSlice';
 import messagesSlice from './slices/messagesSlice';
 import notificationsSlice from './slices/notificationsSlice';
-import searchSlice from './slices/searchSlice';
-import networkSlice from './slices/networkSlice';
-import eventsSlice from './slices/eventsSlice';
-import jobsSlice from './slices/jobsSlice';
-import mentorshipSlice from './slices/mentorshipSlice';
-import messagesSlice from './slices/messagesSlice';
-import notificationsSlice from './slices/notificationsSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
     posts: postsSlice,
     ui: uiSlice,
-    search: searchSlice,
-    network: networkSlice,
-    events: eventsSlice,
-    jobs: jobsSlice,
-    mentorship: mentorshipSlice,
-    messages: messagesSlice,
-    notifications: notificationsSlice,
     search: searchSlice,
     network: networkSlice,
     events: eventsSlice,
